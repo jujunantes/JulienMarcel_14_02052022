f84dc80b95343d8c5b007768f4b7592bc2b9182f
Julien MARCEL <jul_marcel@hotmail.com>
2022-05-20 09:49:36 +0200

    Nettoyage

--- src/utils/Router.jsx
+++ src/utils/Router.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Accueil from '../pages/Accueil'
 import ListeEmployes from '../pages/ListeEmployes'
-import AjouteEmploye from '../pages/AjouteEmploye'
 import Erreur404 from '../composants/Erreur404'
 
 export default function MyRouter() {
@@ -10,7 +9,6 @@ export default function MyRouter() {
             <Routes>
                 <Route path="/" element={<Accueil />} />
                 <Route path="/liste-employes" element={<ListeEmployes />} />
-                <Route path="/ajoute-employe" element={<AjouteEmploye />} />
                 <Route path="*" element={<Erreur404 />} />
             </Routes>
         </Router>
